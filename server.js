7332edd41373049038cd7e891c882fbc7c2ea6c7
sha946 <shayymaromdhane@gmail.com>
2026-03-07 11:29:26 +0100

    fi10

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ const cors     = require("cors");
 const app = express();
 
 /* ---------- MIDDLEWARE ---------- */
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
+app.options("*", cors());
 app.use(express.json({ limit: "2mb" }));
 
 /* ---------- ROUTES ---------- */
